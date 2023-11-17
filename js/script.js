fc7dd0bcdfcaebc9d81e9e2a3d7885e732c84f50
soniamartinezz <sonia93mp@gmail.com>
2023-11-17 21:55:00 +0100

    nueva llamada API + ampliación Pokémon en select

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 let botonBuscar = document.getElementById('get-pokemon');
 let resultado = document.getElementById('resultado');
+let selectBuscar = document.getElementById('pokemon-select');
 
+//Función para obtener todos los Pokémos
+const getAllPokemon = () => {
+
+    //Llamada a API
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+        .then ((response) => {
+            if(!response.ok) {
+                throw new Error('La solicitud no fue exitosa');
+            }
+            return response.json();
+        })
+        .then((data) => {
+
+            //Con los datos obtenidos, añadimos cada Pokémos como option al select
+            for(let i=0; i < data.results.length; i++) {
+                let option = document.createElement('option');
+                option.value = data.results[i].name;
+                option.innerHTML = data.results[i].name;
+                selectBuscar.appendChild(option);
+            }
+        })
+        .catch((error) => console.error('Error:', error));
+}
+
+//Llamamos a función
+getAllPokemon();
+
+//Evento 'click' asociado al botón 'Obtener información'
 botonBuscar.addEventListener("click", function() {
-    let selectBuscar = document.getElementById('pokemon-select');
+    
     let pokemon = selectBuscar.value;
 
+    //Limpiamos DOM
+    resultado.innerHTML = '';
+
+    //Vamos creando nuevo DOM según los resultados
     resultado.innerHTML += `
-        <h3>${pokemon}</h3>
+        <h3>${pokemon.charAt(0).toUpperCase()}${pokemon.slice(1)}</h3>
         <h4>Habilidades:</h4>
         <ul>
     `;
@@ -19,21 +52,25 @@ botonBuscar.addEventListener("click", function() {
             return response.json();
         })
         .then((data) => {
-
-            for(let i=0; i < data.abilities.length; i++) {
-
+            //Comprobamos si el Pokémon tiene habilidades para mostrar un resultado u otro
+            if (data.abilities.length > 0) {
+                for(let i=0; i < data.abilities.length; i++) {
+                    resultado.innerHTML += `
+                        <li>${data.abilities[i].ability.name.charAt(0).toUpperCase()}${data.abilities[i].ability.name.slice(1)}</li>
+                    `;
+                }
+            } else {
                 resultado.innerHTML += `
-                    <li>${data.abilities[i].ability.name}</li>
+                    <li>No tiene habilidades...</li>
                 `;
-            }
-
+            }            
         })
         .catch((error) => console.error('Error:', error));
 
         resultado.innerHTML += `
             </ul>
         `;
-   
+        
 })
 
 
